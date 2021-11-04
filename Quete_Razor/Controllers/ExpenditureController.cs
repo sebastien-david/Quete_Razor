c9edcab5f086cb4e4fd72d588998972b80a3cd55
Sebastien David <sebastien.david08@gmail.com>
2021-11-04 13:40:20 +0100

    Ajoutez des fichiers projet.

---
+++ Quete_Razor/Controllers/ExpenditureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quete_Razor.Models;
+
+namespace Quete_Razor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Expenditure> expendlist = new List<Expenditure>
+            {
+                new Expenditure ("Iphone", new DateTime(2017,02,08), 890.90),
+                new Expenditure ("Samsung", new DateTime(2018,05,10), 799.90),
+                new Expenditure ("Xiaomi", new DateTime(2019,06,12), 570.50)
+            };
+            return View(expendlist);
+        }
+    }
+}
---
+++ Quete_Razor/Models/Expenditure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quete_Razor.Models
+{
+    public class Expenditure
+    {
+        public string Nom { get; set; }
+        public DateTime Date { get; set; }
+        public double Prix { get; set; }
+
+        public Expenditure(string nom, DateTime date, double prix)
+        {
+            Nom = nom;
+            Date = date;
+            Prix = prix;
+        }
+    }
+}
